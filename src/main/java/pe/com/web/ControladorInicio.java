4438e10de77fdccb1d5fa7bd6ea83d14e074ee89
Steven <stevennavarrop@gmail.com>
2023-09-13 16:56:03 -0500

    Internacionalizacion

--- src/main/java/pe/com/web/ControladorInicio.java
+++ src/main/java/pe/com/web/ControladorInicio.java
@@ -1,9 +1,11 @@
 package pe.com.web;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import pe.com.domain.Persona;
@@ -30,7 +32,10 @@ public class ControladorInicio {
     }
     
     @PostMapping("/guardar")
-    public String guardar(Persona persona) {
+    public String guardar(@Valid Persona persona, Errors errores) {
+        if(errores.hasErrors()) {
+            return "modificar";
+        }
         personaService.guardar(persona);
         return "redirect:/";
     }
